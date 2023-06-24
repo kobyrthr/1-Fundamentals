cb4bd59edee8966885319655f2fef13735b17b6c
kobyrthr-dev <kobyrthr@gmail.com>
2023-06-24 12:47:51 -0400

    added credential check

--- workshop2/app.py
+++ workshop2/app.py
@@ -15,3 +15,13 @@ name = input("Enter name to register: ")
 pin = input("Enter PIN: ")
 balance = 0
 print(name + " has been registered with a starting balance of $"+str(balance))
+atm_menu(name)
+while True:
+    name_to_validate = input("What is your user name?")
+    pin_to_validate = input("What is your user pin?")
+    if name_to_validate == name and pin_to_validate == pin:
+        print("Login successful!")
+        break
+    else:
+        print("Invalid credentials")
+        continue

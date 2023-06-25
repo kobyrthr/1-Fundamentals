99cf7494f5ed7592a3a9da504a075e2777683dd5
kobyrthr-dev <kobyrthr@gmail.com>
2023-06-25 02:03:35 -0400

    dice value obtained

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -9,9 +9,9 @@ def dice_game():
     print("2) Leave Game")
     player_choice = input("Enter your choice: ")
     if player_choice == "1":
-        die_1 = random.randint(0,6)
-        die_2 = random.randint(0,6)
-        print(die_1,die_2)
+        die_1 = random.randint(0, 6)
+        die_2 = random.randint(0, 6)
+        print("you rolled a..." + str(die_1) + "\n" + "you rolled a..." + str(die_2) + "\n" + "you have rolled a total of " + str(die_1+die_2))
         dice_game()
     elif player_choice == "2":
         print("Goodbye!")

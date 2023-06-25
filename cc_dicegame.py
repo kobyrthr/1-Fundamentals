fc28b2c5bc6f2af5d6f83994882f9aaeba31848d
kobyrthr-dev <kobyrthr@gmail.com>
2023-06-25 01:59:51 -0400

    set up primary game functions

--- cc_dicegame.py
+++ cc_dicegame.py
@@ -4,10 +4,18 @@ high_score = 0
 
 
 def dice_game():
-    print("Current High Score is: "+ str(high_score))
+    print("Current High Score is: " + str(high_score))
     print("1) Roll Dice")
     print("2) Leave Game")
     player_choice = input("Enter your choice: ")
+    if player_choice == "1":
+        die_1 = random.randint(0,6)
+        die_2 = random.randint(0,6)
+        print(die_1,die_2)
+        dice_game()
+    elif player_choice == "2":
+        print("Goodbye!")
+        quit()
 
 
 dice_game()
